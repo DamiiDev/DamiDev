d3881ec1adaba54e7a3aceac0d89d709a4a40644
Damilare Festus <damilarefestus12@gmail.com>
2026-04-12 23:24:26 +0100

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { FiMoon, FiSun } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem("darkMode") === "true";
+    setIsDarkMode(savedMode);
+  }, []);
+
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-mode", isDarkMode);
+    localStorage.setItem("darkMode", isDarkMode);
+  }, [isDarkMode]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div className="header">
+      <div className="hamburg">
+        <div className="logo">
+          <h1>Festus Damilare</h1>
+        </div>
+        <div className="nav">
+          <a href="#">Home</a>
+          <a href="#">Projects</a>
+          <a href="#">Experience</a>
+          <a href="#">Education</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="download-darkMode">
+          <div className="desktop-download">
+            <a href="/Festus Damilare Michael Re.pdf" download>
+              Download Resume
+            </a>
+          </div>
+          <div className="dark-mode">
+            <button onClick={() => setIsDarkMode((d) => !d)}>
+              {isDarkMode ? <FiSun size={24} /> : <FiMoon size={24} />}
+            </button>
+          </div>
+        </div>
+        <div className="menu-icon" onClick={() => setIsOpen(true)}>
+          <FiMenu size={24} />
+        </div>
+      </div>
+
+      <div className={`mobile-nav ${isOpen ? "open" : ""}`}>
+        <span className="close-btn" onClick={() => setIsOpen(false)}>
+          <FiX size={24} />
+        </span>
+        <a href="#" onClick={() => setIsOpen(false)}>
+          Home
+        </a>
+        <a href="#" onClick={() => setIsOpen(false)}>
+          Projects
+        </a>
+        <a href="#" onClick={() => setIsOpen(false)}>
+          Experience
+        </a>
+        <a href="#" onClick={() => setIsOpen(false)}>
+          Education
+        </a>
+        <a href="#" onClick={() => setIsOpen(false)}>
+          Contact
+        </a>
+        <div className="mobile-download">
+          <a href="/Festus Damilare Michael Re.pdf" download>
+            Download Resume
+          </a>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="overlay" onClick={() => setIsOpen(false)}></div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
